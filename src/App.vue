05bddc7e92fc3bf1bfb1223908331f05518588e4
John Paxton <pax@speedingplanet.com>
2019-08-01 08:54:08 -0500

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <div id="nav" class="text-center">
+    <!--     <div id="nav" class="text-center">
       <router-link to="/">Home</router-link>&nbsp;|
       <router-link to="/playground">Playground</router-link>&nbsp;|
       <router-link to="/about">About</router-link>&nbsp;|
@@ -9,9 +9,21 @@
       <router-link to="/people">People</router-link>&nbsp;|
       <router-link to="/tx">Transactions</router-link>
     </div>
+    -->
+    <Navbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar';
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 
 <style lang="scss">
 @import url('./assets/bootstrap/bootstrap.css');
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary text-white">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" exact-active-class="active" to="/">
+            Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/playground">Playground</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/categories">Categories</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/payees">Payees</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/people">People</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/tx">Transactions</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
